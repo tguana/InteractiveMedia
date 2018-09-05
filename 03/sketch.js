41045df4aef1e15e442505cb94b41e3486aa1552
tguana <42587696+tguana@users.noreply.github.com>
2018-09-04 19:23:37 -0500

    Update sketch.js

--- 03/sketch.js
+++ 03/sketch.js
@@ -26,7 +26,7 @@ var fy = 10;
 function setup() {
 
   // Función que crea el canvas y determina su tamaño.
-  createCanvas(390, 380);
+  createCanvas(windowWidth, windowHeight);
   // Función que determina la velocidad de frames por segundo.
   frameRate(30);
 }
@@ -40,62 +40,62 @@ function draw() {
   // Función que determina el color del relleno.
   fill(255, 102, 0);
   // Conjunto de ellipses que conforman los planetas.
-  ellipse(250, py - 50, 50, 50);
+  ellipse(250*width/390, (py - 50)*height/380, 50*width/390, 50*height/380);
   fill(153, 0, 153);
-  ellipse(400, py - 300, 200, 200);
+  ellipse(400*width/390, (py - 300)*height/380, 200*width/390, 200*height/380);
   fill(51, 204, 51);
-  ellipse(30, py - 30, 150, 150);
+  ellipse(30*width/390, (py - 30)*height/380, 150*width/390, 150*height/380);
   fill(102, 153, 153);
-  ellipse(150, py - 400, 100, 100);
+  ellipse(150*width/390, (py - 400)*height/380, 100*width/390, 100*height/380);
   fill(151, 204, 51);
-  ellipse(250, py - 600, 150, 150);
+  ellipse(250*width/390, (py - 600)*height/380, 150*width/390, 150*height/380);
 	// Función que determina que no debe haber contorno.
   noStroke();
   // Con junto de rectangulos que conforman las "lineas de velocidad".
   fill(255);
-  rect(40, ry - 40, 3, 30);
-  rect(200 - rx, ry - 40, 3, 30);
-  rect(340 + rx, ry - 40, 3, 30);
-  rect(150, ry - 50, 3, 30);
-  rect(100, ry - 50, 3, 30);
-  rect(250 + rx, ry - 100, 3, 30);
-  rect(280 - rx, ry - 40, 3, 30);
-  rect(40 - rx, ry - 100, 3, 30);
-  rect(200, ry - 50, 3, 30);
-  rect(340, ry - 40, 3, 30);
-  rect(150, ry - 100, 3, 30);
-  rect(100 + rx, ry - 50, 3, 30);
-  rect(250, ry - 100, 3, 30);
-  rect(280, ry - 100, 3, 30);
-  rect(40 + rx, ry - 60, 3, 30);
-  rect(200, ry - 50, 3, 30);
-  rect(340, ry - 40, 3, 30);
-  rect(150, ry - 100, 3, 30);
-  rect(100, ry - 40, 3, 30);
-  rect(250 - rx, ry - 50, 3, 30);
-  rect(280 + rx, ry - 50, 3, 30);
-  rect(40, ry - 60, 3, 30);
-  rect(200 - rx, ry - 70, 3, 30);
-  rect(340 + rx, ry - 50, 3, 30);
-  rect(150, ry - 60, 3, 30);
-  rect(100, ry - 70, 3, 30);
-  rect(250 + rx, ry - 90, 3, 30);
-  rect(280 - rx, ry - 60, 3, 30);
-  rect(40 - rx, ry - 80, 3, 30);
-  rect(200, ry - 50, 3, 30);
-  rect(340, ry - 60, 3, 30);
-  rect(150, ry - 100, 3, 30);
-  rect(100 + rx, ry - 70, 3, 30);
-  rect(250, ry - 90, 3, 30);
-  rect(280, ry - 60, 3, 30);
-  rect(40 + rx, ry - 80, 3, 30);
-  rect(200, ry - 70, 3, 30);
-  rect(340, ry - 50, 3, 30);
-  rect(150, ry - 100, 3, 30);
-  rect(100, ry - 90, 3, 30);
-  rect(250 - rx, ry - 80, 3, 30);
-  rect(280 + rx, ry - 60, 3, 30);
-  rect(0, fy - 1000, 400, 900);
+  rect(40*width/390, (ry - 40)*height/380, 3*width/390, 30*height/380);
+  rect((200 - rx)*width/390, (ry - 40)*height/380, 3*width/390, 30*height/380);
+  rect((340 + rx)*width/390, (ry - 40)*height/380, 3*width/390, 30*height/380);
+  rect(150*width/390, (ry - 50)*height/380, 3*width/390, 30*height/380);
+  rect(100*width/390, (ry - 50)*height/380, 3*width/390, 30*height/380);
+  rect((250 + rx)*width/390, (ry - 100)*height/380, 3*width/390, 30*height/380);
+  rect((280 - rx)*width/390, (ry - 40)*height/380, 3*width/390, 30*height/380);
+  rect((40 - rx)*width/390, (ry - 100)*height/380, 3*width/390, 30*height/380);
+  rect(200*width/390, (ry - 50)*height/380, 3*width/390, 30*height/380);
+  rect(340*width/390, (ry - 40)*height/380, 3*width/390, 30*height/380);
+  rect(150*width/390, (ry - 100)*height/380, 3*width/390, 30*height/380);
+  rect((100 + rx)*width/390, (ry - 50)*height/380, 3*width/390, 30*height/380);
+  rect(250*width/390, (ry - 100)*height/380, 3*width/390, 30*height/380);
+  rect(280*width/390, (ry - 100)*height/380, 3*width/390, 30*height/380);
+  rect((40 + rx)*width/390, (ry - 60)*height/380, 3*width/390, 30*height/380);
+  rect(200*width/390, (ry - 50)*height/380, 3*width/390, 30*height/380);
+  rect(340*width/390, (ry - 40)*height/380, 3*width/390, 30*height/380);
+  rect(150*width/390, (ry - 100)*height/380, 3*width/390, 30*height/380);
+  rect(100*width/390, (ry - 40)*height/380, 3*width/390, 30*height/380);
+  rect((250 - rx)*width/390, (ry - 50)*height/380, 3*width/390, 30*height/380);
+  rect((280 + rx)*width/390, (ry - 50)*height/380, 3*width/390, 30*height/380);
+  rect(40*width/390, (ry - 60)*height/380, 3*width/390, 30*height/380);
+  rect((200 - rx)*width/390, (ry - 70)*height/380, 3*width/390, 30*height/380);
+  rect((340 + rx)*width/390, (ry - 50)*height/380, 3*width/390, 30*height/380);
+  rect(150*width/390, (ry - 60)*height/380, 3*width/390, 30*height/380);
+  rect(100*width/390, (ry - 70)*height/380, 3*width/390, 30*height/380);
+  rect((250 + rx)*width/390, (ry - 90)*height/380, 3*width/390, 30*height/380);
+  rect((280 - rx)*width/390, (ry - 60)*height/380, 3*width/390, 30*height/380);
+  rect((40 - rx)*width/390, (ry - 80)*height/380, 3*width/390, 30*height/380);
+  rect(200*width/390, (ry - 50)*height/380, 3*width/390, 30*height/380);
+  rect(340*width/390, (ry - 60)*height/380, 3*width/390, 30*height/380);
+  rect(150*width/390, (ry - 100)*height/380, 3*width/390, 30*height/380);
+  rect((100 + rx)*width/390, (ry - 70)*height/380, 3*width/390, 30*height/380);
+  rect(250*width/390, (ry - 90)*height/380, 3*width/390, 30*height/380);
+  rect(280*width/390, (ry - 60)*height/380, 3*width/390, 30*height/380);
+  rect((40 + rx)*width/390, (ry - 80)*height/380, 3*width/390, 30*height/380);
+  rect(200*width/390, (ry - 70)*height/380, 3*width/390, 30*height/380);
+  rect(340*width/390, (ry - 50)*height/380, 3*width/390, 30*height/380);
+  rect(150*width/390, (ry - 100)*height/380, 3*width/390, 30*height/380);
+  rect(100*width/390, (ry - 90)*height/380, 3*width/390, 30*height/380);
+  rect((250 - rx)*width/390, (ry - 80)*height/380, 3*width/390, 30*height/380);
+  rect((280 + rx)*width/390, (ry - 60)*height/380, 3*width/390, 30*height/380);
+  rect(0*width/390, (fy - 1000)*height/380, 400*width/390, 900*height/380);
   // Función que determina el incremento en la variable py "+3".
   py = py + 3;
   // Función condicional que determina el incremento en las variables py,ay,ry,fy sí el frameCount es mayor que 195.
@@ -110,16 +110,16 @@ function draw() {
   // Función que determina el color del relleno.
   fill(255, 102, 0)
   // Funcion que crea la llama de la nave.
-  ellipse(x, y2 + 10, tam, tam);
+  ellipse(x*width/390, (y2 + 10)*height/380, tam*width/390, tam*height/380);
   stroke(255, 204, 0);
   // Funcion que determina el color del relleno.
   fill(204, 0, 0);
   // Función que crea el exterior de la nave.
   beginShape();
-  vertex(x, y - 15);
-  vertex(x1 + 10, y1 + 10);
-  vertex(x, y2 + 10);
-  vertex(x2 - 10, y1 + 10);
+  vertex(x*width/390, (y - 15)*height/380);
+  vertex((x1 + 10)*width/390, (y1 + 10)*height/380);
+  vertex(x*width/390, (y2 + 10)*height/380);
+  vertex((x2 - 10)*width/390, (y1 + 10)*height/380);
   endShape(CLOSE);
   // Función que determina que no debe haber contorno.
   noStroke();
@@ -127,10 +127,10 @@ function draw() {
   fill(0, 0, 204);
   //Función que crea el interior de la nave.
   beginShape();
-  vertex(x, y);
-  vertex(x1, y1);
-  vertex(x, y2);
-  vertex(x2, y1);
+  vertex(x*width/390, y*height/380);
+  vertex(x1*width/390, y1*height/380);
+  vertex(x*width/390, y2*height/380);
+  vertex(x2*width/390, y1*height/380);
   endShape(CLOSE);
   // Función condicional que determina el incremento de la dirección lateral de la nave si el frameCount se encuentra entre 20 y 175.
   if (frameCount < 175 && frameCount > 20) {
@@ -173,107 +173,107 @@ function draw() {
   fill(102, 51, 0);
   // Conjunto de funciones que crean el asteroide #1.
   beginShape();
-  vertex(160 + 5, 109 + ay - 80);
-  vertex(146 + 5, 36 + ay - 80);
-  vertex(103 + 5, 12 + ay - 80);
-  vertex(69 + 5, 36 + ay - 80);
-  vertex(31 + 5, 93 + ay - 80);
-  vertex(54 + 5, 110 + ay - 80);
-  vertex(109 + 5, 103 + ay - 80);
+  vertex((160 + 5)*width/390, (109 + ay - 80)*height/380);
+  vertex((146 + 5)*width/390, (36 + ay - 80)*height/380);
+  vertex((103 + 5)*width/390, (12 + ay - 80)*height/380);
+  vertex((69 + 5)*width/390, (36 + ay - 80)*height/380);
+  vertex((31 + 5)*width/390, (93 + ay - 80)*height/380);
+  vertex((54 + 5)*width/390, (110 + ay - 80)*height/380);
+  vertex((109 + 5)*width/390, (103 + ay - 80)*height/380);
   endShape(CLOSE);
   fill(153, 102, 51);
   beginShape();
-  vertex(155 + 5, 104 + ay - 80);
-  vertex(140 + 5, 30 + ay - 80);
-  vertex(100 + 5, 20 + ay - 80);
-  vertex(40 + 5, 70 + ay - 80);
-  vertex(40 + 5, 90 + ay - 80);
-  vertex(50 + 5, 100 + ay - 80);
-  vertex(103 + 5, 100 + ay - 80);
+  vertex((155 + 5)*width/390, (104 + ay - 80)*height/380);
+  vertex((140 + 5)*width/390, (30 + ay - 80)*height/380);
+  vertex((100 + 5)*width/390, (20 + ay - 80)*height/380);
+  vertex((40 + 5)*width/390, (70 + ay - 80)*height/380);
+  vertex((40 + 5)*width/390, (90 + ay - 80)*height/380);
+  vertex((50 + 5)*width/390, (100 + ay - 80)*height/380);
+  vertex((103 + 5)*width/390, (100 + ay - 80)*height/380);
   endShape(CLOSE);
   // Conjunto de funciones que crean el asteroide #2.
   fill(102, 51, 0);
   beginShape();
-  vertex(345, 109 + ay - 205);
-  vertex(386, 56 + ay - 205);
-  vertex(323, 12 + ay - 205);
-  vertex(289, 36 + ay - 205);
-  vertex(251, 93 + ay - 205);
-  vertex(274, 110 + ay - 205);
-  vertex(329, 103 + ay - 205);
+  vertex(345*width/390, (109 + ay - 205)*height/380);
+  vertex(386*width/390, (56 + ay - 205)*height/380);
+  vertex(323*width/390, (12 + ay - 205)*height/380);
+  vertex(289*width/390, (36 + ay - 205)*height/380);
+  vertex(251*width/390, (93 + ay - 205)*height/380);
+  vertex(274*width/390, (110 + ay - 205)*height/380);
+  vertex(329*width/390, (103 + ay - 205)*height/380);
   endShape(CLOSE);
   fill(153, 102, 51);
   beginShape();
-  vertex(340, 104 + ay - 205);
-  vertex(380, 50 + ay - 205);
-  vertex(320, 20 + ay - 205);
-  vertex(260, 70 + ay - 205);
-  vertex(260, 90 + ay - 205);
-  vertex(270, 100 + ay - 205);
-  vertex(323, 100 + ay - 205);
+  vertex(340*width/390, (104 + ay - 205)*height/380);
+  vertex(380*width/390, (50 + ay - 205)*height/380);
+  vertex(320*width/390, (20 + ay - 205)*height/380);
+  vertex(260*width/390, (70 + ay - 205)*height/380);
+  vertex(260*width/390, (90 + ay - 205)*height/380);
+  vertex(270*width/390, (100 + ay - 205)*height/380);
+  vertex(323*width/390, (100 + ay - 205)*height/380);
   endShape(CLOSE);
   // Conjunto de funciones que crean el asteroide #3.
   fill(102, 51, 0);
   beginShape();
-  vertex(135, 79 + ay - 300);
-  vertex(166, 36 + ay - 300);
-  vertex(83, 12 + ay - 300);
-  vertex(19, 23 + ay - 300);
-  vertex(01, 93 + ay - 300);
-  vertex(24, 110 + ay - 300);
-  vertex(89, 96 + ay - 300);
+  vertex(135*width/390, (79 + ay - 300)*height/380);
+  vertex(166*width/390, (36 + ay - 300)*height/380);
+  vertex(83*width/390, (12 + ay - 300)*height/380);
+  vertex(19*width/390, (23 + ay - 300)*height/380);
+  vertex(01*width/390, (93 + ay - 300)*height/380);
+  vertex(24*width/390, (110 + ay - 300)*height/380);
+  vertex(89*width/390, (96 + ay - 300)*height/380);
   endShape(CLOSE);
   fill(153, 102, 51);
   beginShape();
-  vertex(130, 74 + ay - 300);
-  vertex(160, 30 + ay - 300);
-  vertex(80, 20 + ay - 300);
-  vertex(20, 30 + ay - 300);
-  vertex(10, 90 + ay - 300);
-  vertex(20, 100 + ay - 300);
-  vertex(83, 90 + ay - 300);
+  vertex(130*width/390, (74 + ay - 300)*height/380);
+  vertex(160*width/390, (30 + ay - 300)*height/380);
+  vertex(80*width/390, (20 + ay - 300)*height/380);
+  vertex(20*width/390, (30 + ay - 300)*height/380);
+  vertex(10*width/390, (90 + ay - 300)*height/380);
+  vertex(20*width/390, (100 + ay - 300)*height/380);
+  vertex(83*width/390, (90 + ay - 300)*height/380);
   endShape(CLOSE);
   // Conjunto de funciones que crean el asteroide #4.
   fill(102, 51, 0);
   beginShape();
-  vertex(115, 129 + ay - 500);
-  vertex(166, 16 + ay - 500);
-  vertex(133, 2 + ay - 500);
-  vertex(19, 16 + ay - 500);
-  vertex(21, 53 + ay - 500);
-  vertex(74, 100 + ay - 500);
-  vertex(79, 93 + ay - 500);
+  vertex(115*width/390, (129 + ay - 500)*height/380);
+  vertex(166*width/390, (16 + ay - 500)*height/380);
+  vertex(133*width/390, (2 + ay - 500)*height/380);
+  vertex(19*width/390, (16 + ay - 500)*height/380);
+  vertex(21*width/390, (53 + ay - 500)*height/380);
+  vertex(74*width/390, (100 + ay - 500)*height/380);
+  vertex(79*width/390, (93 + ay - 500)*height/380);
   endShape(CLOSE);
   fill(153, 102, 51);
   beginShape();
-  vertex(112, 124 + ay - 500);
-  vertex(160, 13 + ay - 500);
-  vertex(120, 10 + ay - 500);
-  vertex(21, 12 + ay - 500);
-  vertex(25, 50 + ay - 500);
-  vertex(77, 95 + ay - 500);
-  vertex(85, 85 + ay - 500);
+  vertex(112*width/390, (124 + ay - 500)*height/380);
+  vertex(160*width/390, (13 + ay - 500)*height/380);
+  vertex(120*width/390, (10 + ay - 500)*height/380);
+  vertex(21*width/390, (12 + ay - 500)*height/380);
+  vertex(25*width/390, (50 + ay - 500)*height/380);
+  vertex(77*width/390, (95 + ay - 500)*height/380);
+  vertex(85*width/390, (85 + ay - 500)*height/380);
   endShape(CLOSE);
   // Conjunto de funciones que crean el asteroide #5.
   fill(102, 51, 0);
   beginShape();
-  vertex(345, 129 + ay - 500);
-  vertex(386, 16 + ay - 500);
-  vertex(323, 12 + ay - 500);
-  vertex(289, 16 + ay - 500);
-  vertex(251, 53 + ay - 500);
-  vertex(245, 110 + ay - 500);
-  vertex(329, 93 + ay - 500);
+  vertex(345*width/390, (129 + ay - 500)*height/380);
+  vertex(386*width/390, (16 + ay - 500)*height/380);
+  vertex(323*width/390, (12 + ay - 500)*height/380);
+  vertex(289*width/390, (16 + ay - 500)*height/380);
+  vertex(251*width/390, (53 + ay - 500)*height/380);
+  vertex(245*width/390, (110 + ay - 500)*height/380);
+  vertex(329*width/390, (93 + ay - 500)*height/380);
   endShape(CLOSE);
   fill(153, 102, 51);
   beginShape();
-  vertex(340, 124 + ay - 500);
-  vertex(380, 13 + ay - 500);
-  vertex(320, 10 + ay - 500);
-  vertex(260, 12 + ay - 500);
-  vertex(260, 50 + ay - 500);
-  vertex(255, 105 + ay - 500);
-  vertex(323, 85 + ay - 500);
+  vertex(340*width/390, (124 + ay - 500)*height/380);
+  vertex(380*width/390, (13 + ay - 500)*height/380);
+  vertex(320*width/390, (10 + ay - 500)*height/380);
+  vertex(260*width/390, (12 + ay - 500)*height/380);
+  vertex(260*width/390, (50 + ay - 500)*height/380);
+  vertex(255*width/390, (105 + ay - 500)*height/380);
+  vertex(323*width/390, (85 + ay - 500)*height/380);
   endShape(CLOSE);
   // Función que determina el incremento de la variable "ay" en +4.
   ay = ay + 4;
@@ -281,5 +281,11 @@ function draw() {
   if (frameCount > 240) {
     //
     background(255);
-  }
+   }
+  // //
+  // if(frameCount<=240){
+  // if(frameCount % 2 == 0){
+  //  saveCanvas("FlipBook" + frameCount, 'jpg');
+  // }
+  // }
 }
