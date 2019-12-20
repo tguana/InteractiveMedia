f6ad9f8f777166ce5343141047e350dd7fbe84c5
tguana <42587696+tguana@users.noreply.github.com>
2019-12-19 19:15:49 -0500

    Update sketch.js

--- 02/sketch.js
+++ 02/sketch.js
@@ -24,48 +24,48 @@ function setup()
   //
   var y = 245;
   
-  //Recorre la 1er fila aumentando 140 en p.
+  // Pushes the 1st row increasing 140 in p.
  	for(var p = 0; p<150; p = p+s3)
   {
-    //Determina el color del borde y relleno.
+    // Determines the color of the border and fill.
     noStroke();
     fill(0);
 
-    //Crea los trangulos en la 1er fila sup.
+    //Creates the triangles in the 1st row sup.
     triangle(330-p,50,295-p,50,330-p,85);
     triangle(330-p-s,50,295-p-s,50,330-p-s,85); 
 
-    //Crea los trangulos en la 1er fila inf.
+    // Creates the triangles in the 1st row inf.
     triangle(50+p,50,85+p,85,50+p,85);
     triangle(50+p+s,50,85+p+s,85,50+p+s,85);
 
-    //Crea los trangulos en la 2da fila sup.
+    // Creates the triangles in the 2nd row sup.
     triangle(330-p-s,50+s,295-p-s,50+s,330-p-s,85+s);
     triangle(330-p-s1,50+s,295-p-s1,50+s,330-p-s1,85+s);
     
-    //Crea los trangulos en la 5ta fila sup.
+    // Creates the triangles in the 5th row sup.
     triangle(330-p,50+s3,295-p,50+s3,330-p,85+s3);
     triangle(330-p-s,50+s3,295-p-s,50+s3,330-p-s,85+s3); 
 
-    //Crea los trangulos en la 5ta fila inf.
+    // Creates the triangles in the 5th row inf.
     triangle(50+p,50+s3,85+p,85+s3,50+p,85+s3);
     triangle(50+p+s,50+s3,85+p+s,85+s3,50+p+s,85+s3);
     
-    //Crea los trangulos en la 6ta fila sup.
+    // Creates the triangles in the 6th row sup.
     triangle(330-p-s,50+s4,295-p-s,50+s4,330-p-s,85+s4);
     triangle(330-p-s1,50+s4,295-p-s1,50+s4,330-p-s1,85+s4);
     
-    //Crea los triangulos en la 4ta fila sup.
+    // Creates the triangles in the 4th row sup.
     triangle(330-p,50+s2,295-p,50+s2,330-p,85+s2);
     
-    //Crea los triangulos en la 8bta fila sup.
+    // Creates the triangles in the 8th row sup.
     triangle(330-p,50+s6,295-p,50+s6,330-p,85+s6);
   }
   
-  //Recorre la 2da fila aumentando 105 en p.
+  // Pushes the 2nd row increasing 105 in p.
  	for(p = 0; p<150; p = p+105)
   { 
-    //Crea los trangulos en la 2da fila inf.
+    // Creates the triangles in the 2nd row inf.
     triangle(50+p,50+s,85+p,85+s,50+p,85+s);
     triangle(50+p+s3,50+s,85+p+s3,85+s,50+p+s3,85+s);
     
@@ -74,288 +74,288 @@ function setup()
     triangle(50+p+s3,50+s4,85+p+s3,85+s4,50+p+s3,85+s4);
   }
  
-  //Recorre la 3ra fila aumentando 35 en p.
+  // Pushes the 3rd row increasing 35 by p.
  	for(p = 70; p<130; p = p+s)
   { 
-    //Crea los trangulos en la 3ra fila inf.
+    // Creates the triangles in the 3rd row inf.
     triangle(50+p,50+s1,85+p,85+s1,50+p,85+s1);
     triangle(50+p+s3,50+s1,85+p+s3,85+s1,50+p+s3,85+s1);
     
-    //Crea los trangulos en la 3ra fila sup.
+    // Creates the triangles in the 3rd row sup.
 		triangle(330-p,50+s1,295-p,50+s1,330-p,85+s1);
     triangle(330-p-s3,50+s1,295-p-s3,50+s1,330-p-s3,85+s1);
     
-    //Crea los trangulos en la 7ma fila inf.
+    // Creates the triangles in the 7th row inf.
     triangle(50+p,50+s5,85+p,85+s5,50+p,85+s5);
     triangle(50+p+s3,50+s5,85+p+s3,85+s5,50+p+s3,85+s5);
     
-    //Crea los trangulos en la 7ma fila sup.
+    // Creates the triangles in the 7th row sup.
 		triangle(330-p,50+s5,295-p,50+s5,330-p,85+s5);
     triangle(330-p-s3,50+s5,295-p-s3,50+s5,330-p-s3,85+s5);
   }
   
-  //Recorre la 4ta fila aumentando 140 en p.
+  // Pushes the 4th row increasing 140 in p.
   for(p=35; p<250; p = p+140)
   {
-    //Crea los triangulos en la 4ta fila inf.
+    // Creates the triangles in the 4th row inf.
   	triangle(50+p,50+s2,85+p,85+s2,50+p,85+s2);
     triangle(50+p+s,50+s2,85+p+s,85+s2,50+p+s,85+s2);
     
-    //Crea los triangulos en la 4ta fila sup.
+    // Creates the triangles in the 4th row sup.
     triangle(330-p-s1,50+s2,295-p-s1,50+s2,330-p-s1,85+s2);
     
-    //Crea los triangulos en la 8ba fila inf.
+    // Creates the triangles in the 8th row inf.
   	triangle(50+p,50+s6,85+p,85+s6,50+p,85+s6);
     triangle(50+p+s,50+s6,85+p+s,85+s6,50+p+s,85+s6);
     
-    //Crea los triangulos en la 8ba fila sup.
+    // Creates the triangles in the 8th row sup.
     triangle(330-p-s1,50+s6,295-p-s1,50+s6,330-p-s1,85+s6);
   }
   
-  //Recorre la 1er fila superior derecha aumentandoo 140 en p.
+  // Pushes the 1st upper right row increasing or 140 in p.
  	
 	for( p = 0; p<150; p = p+s3)
   {
-    //Crea los trangulos en la 6ta fila sup.
+    // Creates the triangles in the 6th row sup.
     triangle(330+x-p-s,50+s2,295+x-p-s,50+s2,295+x-p-s,85+s2); 
     triangle(330+x-p-s1,50+s2,295+x-p-s1,50+s2,295+x-p-s1,85+s2);
     
-    //Crea los trangulos en la 5ta fila sup.
+    // Creates the triangles in the 5th row sup.
     triangle(330+x-p,50+s1,295+x-p,85+s1,330+x-p,85+s1);
 		triangle(330+x-p-s,50+s1,295+x-p-s,85+s1,330+x-p-s,85+s1); 
     
-    //Crea los trangulos en la 5ta fila inf.
+    // Creates the triangles in the 5th row inf.
     triangle(50+x+p,50+s1,50+x+p,85+s1,85+x+p,50+s1);
     triangle(50+x+p+s,50+s1,50+x+p+s,85+s1,85+x+p+s,50+s1);
     
-    //Crea los triangulos en la 4ta fila sup.
+    // Creates the triangles in the 4th row sup.
 		triangle(330+x-p,50+s,295+x-p,50+s,295+x-p,85+s);
     
-    //Crea los triangulos en la 8bta fila sup.
+    // Creates the triangles in the 8th row sup.
     triangle(330+x-p,50+s4,295+x-p,50+s4,295+x-p,85+s4);
     
-    //Crea los trangulos en la 1er fila sup.
+    // Creates the triangles in the 1st row sup.
     triangle(330+x-p,50+s5,295+x-p,85+s5,330+x-p,85+s5);
     triangle(330+x-p-s,50+s5,295+x-p-s,85+s5,330+x-p-s,85+s5); 
 
-    //Crea los trangulos en la 1er fila inf.
+    // Creates the triangles in the 1st row inf.
     triangle(50+x+p,50+s5,85+x+p,50+s5,50+x+p,85+s5);
     triangle(50+x+p+s,50+s5,85+x+p+s,50+s5,50+x+p+s,85+s5);
     
-    //Crea los trangulos en la 2da fila sup.
+    // Creates the triangles in the 2nd row sup.
     triangle(330+x-p-s,50+s6,295+x-p-s,50+s6,295+x-p-s,85+s6);
     triangle(330+x-p-s1,50+s6,295+x-p-s1,50+s6,295+x-p-s1,85+s6);
   }
   
-  //Recorre la 2da fila superior derecha aumentando 140 en p.
+  // Pushes the 2nd upper right row increasing 140 in p.
   for(p=35; p<250; p = p+140)
   {
-    //Crea los triangulos en la 4ta fila inf.
+    // Creates the triangles in the 4th row inf.
   	triangle(50+x+p,85+s,85+x+p,85+s,85+x+p,50+s);
     triangle(50+x+p+s,85+s,85+x+p+s,85+s,85+x+p+s,50+s);
     
-    //Crea los triangulos en la 4ta fila sup.
+    // Creates the triangles in the 4th row sup.
     triangle(330+x-p-s1,50+s,295+x-p-s1,50+s,295+x-p-s1,85+s);
     
-    //Crea los triangulos en la 8ba fila inf.
+    // Creates the triangles in the 8th row inf.
   	triangle(85+x+p,50+s4,85+x+p,85+s4,50+x+p,85+s4);
     triangle(85+x+p+s,50+s4,85+x+p+s,85+s4,50+x+p+s,85+s4);
     
-    //Crea los triangulos en la 8ba fila sup.
+    // Creates the triangles in the 8th row sup.
     triangle(330+x-p-s1,50+s4,295+x-p-s1,50+s4,295+x-p-s1,85+s4);
   }
     
-  //Recorre la 2da fila superior derecha aumentando 35 en p.
+  //  Pushes the 2nd upper right row increasing 35 in p.
  	for(p = 70; p<130; p = p+s)
   {
     
-  //Crea los trangulos en la 3ra fila inf.
+  // Creates the triangles in the 3rd row inf.
     triangle(50+x+p,50,85+x+p,50,50+x+p,85);
     triangle(50+x+p+s3,50,85+x+p+s3,50,50+x+p+s3,85);
     
-    //Crea los trangulos en la 3ra fila sup.
+    // Creates the triangles in the 3rd row sup.
 		triangle(330+x-p,50,295+x-p,85,330+x-p,85);
     triangle(330+x-p-s3,50,295+x-p-s3,85,330+x-p-s3,85);
     
-    //Crea los trangulos en la 7ma fila inf.
+    // Creates the triangles in the 7th row inf.
     triangle(50+x+p,50+s3,85+x+p,50+s3,50+x+p,85+s3);
     triangle(50+x+p+s3,50+s3,85+x+p+s3,50+s3,50+x+p+s3,85+s3);
     
-    //Crea los trangulos en la 7ma fila sup.
+    // Creates the triangles in the 7th row sup.
 		triangle(330+x-p,50+s3,295+x-p,85+s3,330+x-p,85+s3);
     triangle(330+x-p-s3,50+s3,295+x-p-s3,85+s3,330+x-p-s3,85+s3);
   }
   
-  //Recorre la 4ta fila aumentando 105 en p.
+  // Pushes the 4th row increasing 105 in p.
  	for(p = 0; p<150; p = p+105)
   {
-  	//Crea los trangulos en la 6ta fila inf.
+    // Creates the triangles in the 6th row inf.
     triangle(85+x+p,50+s2,85+x+p,85+s2,50+x+p,85+s2);
     triangle(85+x+p+s3,50+s2,85+x+p+s3,85+s2,50+x+p+s3,85+s2);
     
-    //Crea los trangulos en la 2da fila inf.
+    // Creates the triangles in the 2nd row inf.
     triangle(85+x+p,50+s6,85+x+p,85+s6,50+x+p,85+s6);
     triangle(85+x+p+s3,50+s6,85+x+p+s3,85+s6,50+x+p+s3,85+s6);
   }
      
-  //Recorre la 1er fila inferior derecha aumentando 140 en p.
+  // Pushes the 1st lower right row increasing 140 in p.
  	for( p = 0; p<150; p = p+s3)
   {
-    //Determina el color del borde y relleno.
+    // Determines the color of the border and fill.
     noStroke();
     fill(255,204,55);
     
-    //Crea los trangulos en la 2da fila sup.
+    // Creates the triangles in the 2nd row sup.
     triangle(330+x-p-s,50+y+s,295+x-p-s,50+y+s,330+x-p-s,85+y+s);
     triangle(330+x-p-s1,50+y+s,295+x-p-s1,50+y+s,330+x-p-s1,85+y+s);
     
-    //Crea los triangulos en la 4ta fila sup.
+    // Creates the triangles in the 4th row sup.
     triangle(330+x-p,50+y+s2,295+x-p,50+y+s2,330+x-p,85+y+s2);
     
-    //Crea los trangulos en la 5ta fila inf.
+    // Creates the triangles in the 5th row inf.
     triangle(50+x+p,50+y+s3,85+x+p,85+y+s3,50+x+p,85+y+s3);
     triangle(50+x+p+s,50+y+s3,85+x+p+s,85+y+s3,50+x+p+s,85+y+s3);
     
-    //Crea los trangulos en la 5ta fila sup.
+    // Creates the triangles in the 5th row sup.
     triangle(330+x-p,50+y+s3,295+x-p,50+y+s3,330+x-p,85+y+s3);
     triangle(330+x-p-s,50+y+s3,295+x-p-s,50+y+s3,330+x-p-s,85+y+s3);
     
-    //Crea los trangulos en la 6ta fila sup.
+    // Creates the triangles in the 6th row sup.
     triangle(330+x-p-s,50+y+s4,295+x-p-s,50+y+s4,330+x-p-s,85+y+s4);
     triangle(330+x-p-s1,50+y+s4,295+x-p-s1,50+y+s4,330+x-p-s1,85+y+s4);
     
-    //Crea los triangulos en la 8ba fila sup.
+    // Creates the triangles in the 8th row sup.
     triangle(330+x-p,50+y+s6,295+x-p,50+y+s6,330+x-p,85+y+s6);
     
   }  
   
-  //Recorre la 2da fila inferior derecha aumentando 105 en p.
+  // Pushes the 2nd lower right row increasing 105 in p.
  	for(p = 0; p<150; p = p+105)
   { 
-    //Crea los trangulos en la 2da fila inf.
+    // Creates the triangles in the 2nd row inf.
     triangle(50+x+p,50+y+s,85+x+p,85+y+s,50+x+p,85+y+s);
     triangle(50+x+p+s3,50+y+s,85+x+p+s3,85+y+s,50+x+p+s3,85+y+s);
     
-    //Crea los trangulos en la 6ta fila inf.
+    // Creates the triangles in the 6th row inf.
     triangle(50+x+p,50+y+s4,85+x+p,85+y+s4,50+x+p,85+y+s4);
     triangle(50+x+p+s3,50+y+s4,85+x+p+s3,85+y+s4,50+x+p+s3,85+y+s4);
   }
   
-  //Recorre la 3ra fila inferior derecha aumentando 35 en p.
+  // Pushes the 3rd lower right row increasing 35 by p.
  	for(p = 70; p<130; p = p+s)
   { 
-    //Crea los trangulos en la 3ra fila inf.
+    // Creates the triangles in the 3rd row inf.
     triangle(50+x+p,50+y+s1,85+x+p,85+y+s1,50+x+p,85+y+s1);
     triangle(50+x+p+s3,50+y+s1,85+x+p+s3,85+y+s1,50+x+p+s3,85+y+s1);
     
-    //Crea los trangulos en la 3ra fila sup.
+    // Creates the triangles in the 3rd row sup.
 		triangle(330+x-p,50+y+s1,295+x-p,50+y+s1,330+x-p,85+y+s1);
     triangle(330+x-p-s3,50+y+s1,295+x-p-s3,50+y+s1,330+x-p-s3,85+y+s1);
     
-    //Crea los trangulos en la 7ma fila inf.
+    // Creates the triangles in the 7th row inf.
     triangle(50+x+p,50+y+s5,85+x+p,85+y+s5,50+x+p,85+y+s5);
     triangle(50+x+p+s3,50+y+s5,85+x+p+s3,85+y+s5,50+x+p+s3,85+y+s5);
     
-    //Crea los trangulos en la 7ma fila sup.
+    // Creates the triangles in the 7th row sup.
 		triangle(330+x-p,50+y+s5,295+x-p,50+y+s5,330+x-p,85+y+s5);
     triangle(330+x-p-s3,50+y+s5,295+x-p-s3,50+y+s5,330+x-p-s3,85+y+s5);
   }
   
-  //Recorre la 4ta fila inferior derecha aumentando 140 en p.
+  // Pushes the 4th lower right row increasing 140 in p.
   for(p=35; p<250; p = p+140)
   {
-    //Crea los triangulos en la 4ta fila inf.
+    // Creates the triangles in the 4th row inf.
   	triangle(50+x+p,50+y+s2,85+x+p,85+y+s2,50+x+p,85+y+s2);
     triangle(50+x+p+s,50+y+s2,85+x+p+s,85+y+s2,50+x+p+s,85+y+s2);
     
-    //Crea los triangulos en la 4ta fila sup.
+    // Creates the triangles in the 4th row sup.
     triangle(330+x-p-s1,50+y+s2,295+x-p-s1,50+y+s2,330+x-p-s1,85+y+s2);
     
-    //Crea los triangulos en la 8ba fila inf.
+    // Creates the triangles in the 8th row inf.
   	triangle(50+x+p,50+y+s6,85+x+p,85+y+s6,50+x+p,85+y+s6);
     triangle(50+x+p+s,50+y+s6,85+x+p+s,85+y+s6,50+x+p+s,85+y+s6);
     
-    //Crea los triangulos en la 8ba fila sup.
+    // Creates the triangles in the 8th row sup.
     triangle(330+x-p-s1,50+y+s6,295+x-p-s1,50+y+s6,330+x-p-s1,85+y+s6);
   }
   
-  //Recorre la 1er fila inferior izquierda aumentandoo 140 en p.
+  // Pushes the 1st lower left row increasing or 140 in p.
  	
  	for( p = 0; p<150; p = p+s3)
    {
-    //Determina el color del borde y relleno.
+    // Determines the color of the border and fill.
     noStroke();
     fill(0);
      
-    //Crea los trangulos en la 6ta fila sup.
+    // Creates the triangles in the 6th row sup.
   	triangle(330-p-s,50+y+s2,295-p-s,50+y+s2,295-p-s,85+y+s2); 
     triangle(330-p-s1,50+y+s2,295-p-s1,50+y+s2,295-p-s1,85+y+s2);
     
-    //Crea los trangulos en la 5ta fila sup.
+    // Creates the triangles in the 5th row sup.
     triangle(330-p,50+y+s1,295-p,85+y+s1,330-p,85+y+s1);
  		triangle(330-p-s,50+y+s1,295-p-s,85+y+s1,330-p-s,85+y+s1); 
     
-     //Crea los trangulos en la 5ta fila inf.
+     // Creates the triangles in the 5th row inf.
      triangle(50+p,50+y+s1,50+p,85+y+s1,85+p,50+y+s1);
      triangle(50+p+s,50+y+s1,50+p+s,85+y+s1,85+p+s,50+y+s1);
     
-     //Crea los triangulos en la 4ta fila sup.
+     // Creates the triangles in the 4th row sup.
  		triangle(330-p,50+y+s,295-p,50+y+s,295-p,85+y+s);
     
-     //Crea los triangulos en la 8bta fila sup.
+     // Creates the triangles in the 8th row sup.
      triangle(330-p,50+y+s4,295-p,50+y+s4,295-p,85+y+s4);
     
-     //Crea los trangulos en la 1er fila sup.
+     // Creates the triangles in the 1st row sup.
      triangle(330-p,50+s5+y,295-p,85+s5+y,330-p,85+s5+y);
      triangle(330-p-s,50+s5+y,295-p-s,85+s5+y,330-p-s,85+s5+y); 
 
-     //Crea los trangulos en la 1er fila inf.
+     // Creates the triangles in the 1st row inf.
      triangle(50+p,50+s5+y,85+p,50+s5+y,50+p,85+s5+y);
      triangle(50+p+s,50+s5+y,85+p+s,50+s5+y,50+p+s,85+s5+y);
     
-     //Crea los trangulos en la 2da fila sup.
+     // Creates the triangles in the 2nd row sup.
      triangle(330-p-s,50+s6+y,295-p-s,50+s6+y,295-p-s,85+s6+y);
      triangle(330-p-s1,50+s6+y,295-p-s1,50+s6+y,295-p-s1,85+s6+y);
    }
   
-   //Recorre la 2da fila superior derecha aumentando 140 en p.
+   // Pushes the 2nd upper right row increasing 140 in p.
    for(p=35; p<250; p = p+140)
    {
-     //Crea los triangulos en la 4ta fila inf.
+     // Creates the triangles in the 4th row inf.
    	triangle(50+p,85+s+y,85+p,85+s+y,85+p,50+s+y);
      triangle(50+p+s,85+s+y,85+p+s,85+s+y,85+p+s,50+s+y);
     
-     //Crea los triangulos en la 4ta fila sup.
+     // Creates the triangles in the 4th row sup.
      triangle(330-p-s1,50+s+y,295-p-s1,50+s+y,295-p-s1,85+s+y);
     
-     //Crea los triangulos en la 8ba fila inf.
+     // Creates the triangles in the 8th row inf.
    	triangle(85+p,50+s4+y,85+p,85+s4+y,50+p,85+s4+y);
      triangle(85+p+s,50+s4+y,85+p+s,85+s4+y,50+p+s,85+s4+y);
     
-     //Crea los triangulos en la 8ba fila sup.
+     // Creates the triangles in the 8th row sup.
      triangle(330-p-s1,50+s4+y,295-p-s1,50+s4+y,295-p-s1,85+s4+y);
    }
     
-   //Recorre la 2da fila superior derecha aumentando 35 en p.
+   // Pushes the 2nd upper right row increasing 35 in p.
   	for(p = 70; p<130; p = p+s)
    {
-     //Crea los trangulos en la 7ma fila inf.
+     // Creates the triangles in the 7th row inf.
      triangle(50+p,50+s3+y,85+p,50+s3+y,50+p,85+s3+y);
      triangle(50+p+s3,50+s3+y,85+p+s3,50+s3+y,50+p+s3,85+s3+y);
     
-     //Crea los trangulos en la 7ma fila sup.
+     // Creates the triangles in the 7th row sup.
  		triangle(330-p,50+s3+y,295-p,85+s3+y,330-p,85+s3+y);
      triangle(330-p-s3,50+s3+y,295-p-s3,85+s3+y,330-p-s3,85+s3+y);
    }
   
-   //Recorre la 4ta fila aumentando 105 en p.
+   // Pushes the 4th row increasing 105 in p.
   	for(p = 0; p<150; p = p+105)
    {
-   	//Crea los trangulos en la 6ta fila inf.
+     // Creates the triangles in the 6th row inf.
      triangle(85+p,50+s2+y,85+p,85+s2+y,50+p,85+s2+y);
      triangle(85+p+s3,50+s2+y,85+p+s3,85+s2+y,50+p+s3,85+s2+y);
     
-     //Crea los trangulos en la 2da fila inf.
+     // Creates the triangles in the 2nd row inf.
      triangle(85+p,50+s6+y,85+p,85+s6+y,50+p,85+s6+y);
      triangle(85+p+s3,50+s6+y,85+p+s3,85+s6+y,50+p+s3,85+s6+y);
    }
